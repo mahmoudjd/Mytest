50e19dbd2f7f81fd19022220ca85e22573b61dce
mahmoud <mahmoudjd452@gmail.com>
2021-07-04 22:27:02 +0200

    versuch 4

--- Java/Hello.java
+++ Java/Hello.java
@@ -3,6 +3,6 @@ public class Hello
 	// ich habe neuen Komment hinzugefügt 
     public static void main(String [] args)
     {
-        System.out.println("First VsCode Implementation");
+        System.out.println("First ---  VsCode Implementation");
     }
 }
